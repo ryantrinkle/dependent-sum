ac582792d456e372feb0e20e6c90092d7ae5dfe7
Ryan Trinkle <ryan.trinkle@gmail.com>
2014-05-01 19:38:22 +0000

    Add DeriveDataTypeable.

--- src/Data/Dependent/Sum/Typeable.hs
+++ src/Data/Dependent/Sum/Typeable.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE CPP #-}
 #if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 707
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE StandaloneDeriving, DeriveDataTypeable #-}
 #endif
 #if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 702
 {-# LANGUAGE Trustworthy #-}
